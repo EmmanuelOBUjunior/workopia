c8baabc59ff8557ff48bbc745d4568097289ae8b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-07 13:54:14 +0000

    Working on view all jobs component

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,9 @@ const App = () => {
   return (
     <>
     <Navbar/>
-    <Hero
-    
-    />
+    <Hero/>
     <HomeCards/>
     <JobListings/>
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
-
     </>
   )
 }
