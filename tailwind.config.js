d877ad1b75b0194f30547e58f57acf3244c80c96
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-07 10:55:31 +0000

    Setting up workopia project up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: ['Roboto', 'sans-serif']
+      },
+      gridTemplateColumns:{
+        
+      }
+    },
   },
   plugins: [],
 }
