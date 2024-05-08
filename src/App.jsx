aacc025e64c6673e36af1254e056806ed24b470b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-08 08:51:28 +0000

    Working on routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards"
-import JobListings from "./components/JobListings"
-import Navbar from "./components/Navbar"
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 const App = () => {
-  return (
-    <>
-    <Navbar/>
-    <Hero/>
-    <HomeCards/>
-    <JobListings/>
-    </>
-  )
-}
+  return <></>;
+};
 
-export default App
+export default App;
