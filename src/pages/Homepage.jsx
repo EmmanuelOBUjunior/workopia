598ee713bdd585e5fa117698869dc06d02c8595a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-08 09:27:00 +0000

    Working on routing

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,13 @@
 import Hero from '../components/Hero'
+import HomeCards from '../components/HomeCards'
+import Hero from '../components/Hero'
+import Hero from '../components/Hero'
 
 const Homepage = () => {
   return (
     <>
     <Hero/>
+    <HomeCards/>
     </>
   )
 }
