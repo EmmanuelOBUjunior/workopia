6482390534814dfcb1f167441eec07a4025f39f7
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-08 09:28:33 +0000

    Working on routing

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,14 @@
 import Hero from '../components/Hero'
 import HomeCards from '../components/HomeCards'
 import ViewAllJobs from '../components/ViewAllJobs'
-import JobListings from '../components/Hero'
+import JobListings from '../components/JobListings'
 
 const Homepage = () => {
   return (
     <>
     <Hero/>
     <HomeCards/>
+    <ViewAllJobs></ViewAllJobs>
     </>
   )
 }
