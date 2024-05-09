857d6055f71e8c938d22fcc24d89905a1517a823
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 08:24:44 +0000

    Working on custom not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -5,8 +5,6 @@ const NotFoundPage = () => {
   return (
     <section>
         <div>
-            
-        </div>
       <FaExclamationTriangle className=" text-yellow-400 text-6xl mb-4"/>
       <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
       <p className="text-xl mb-5">Sorry!!! The page you are looking for does not exist</p>
@@ -16,6 +14,7 @@ const NotFoundPage = () => {
       >
         Go Back To Home
       </Link>
+        </div>
     </section>
   );
 };
