8d08dc3486899da96eac0b71cf16d744b790e6f0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 08:11:29 +0000

    Working on routes

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -34,12 +34,12 @@ const JobListing = ({job}) => {
                 <FaMapMarker/>
                 {job.location}
               </div>
-              <a
+              <Link
                 href={`/job/${job.id}`}
                 className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
               >
                Read More
-              </a>
+              </Link>
             </div> 
           </div>
         </div>
