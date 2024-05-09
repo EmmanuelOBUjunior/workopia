847c60a1dc67d60979f4e45666e5d86c7daa32d0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 14:17:35 +0000

    Linking backend with frontend

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -5,7 +5,7 @@ import JobListing from '../components/JobListing'
 
 const JobListings = ({isHome=false}) => {
   const [jobs, setJobs] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   useEffect(()=>{
     const fetchJobs = async()=>{
@@ -16,7 +16,7 @@ const JobListings = ({isHome=false}) => {
       } catch (error) {
         console.log("Error fetching data!!", error);
       }finally{
-        setLoading(false)
+        setLoading(true)
       }
       
     }
