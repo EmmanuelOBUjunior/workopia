d38b78fffa2056cc8b3f01f6bdc3af9282894fd5
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 08:24:34 +0000

    Working on custom not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,7 +3,10 @@ import { FaExclamationTriangle } from "react-icons/fa";
 
 const NotFoundPage = () => {
   return (
-    <section className="text-center flex flex-col justify-center items-center">
+    <section>
+        <div>
+            
+        </div>
       <FaExclamationTriangle className=" text-yellow-400 text-6xl mb-4"/>
       <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
       <p className="text-xl mb-5">Sorry!!! The page you are looking for does not exist</p>
