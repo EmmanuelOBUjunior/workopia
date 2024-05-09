6d46ad7beb5e04d2e19cdb7d27a3d7ca3201dd5b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 08:16:44 +0000

    Working on custom not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
@@ -6,14 +6,14 @@ const NotFoundPage = () => {
       <i className="fas fa-exclamation-triangle text-yellow-400 fa-4x mb-4"></i>
       <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
       <p className="text-xl mb-5">This page does not exist</p>
-      <a
-        href="/"
+      <Link
+        to="/"
         className="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
-        >Go Back</a
       >
+        Go Back
+      </Link>
     </section>
+  );
+};
 
-  )
-}
-
-export default NotFoundPage
+export default NotFoundPage;
