a4fd0ad4b9bbfdd808343893ea5695905b4860ec
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 09:22:17 +0000

    Working on main jobs page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,7 +1,9 @@
 import jobs from '../../src/jobs.json'
 import JobListing from './JobListing'
 
-const JobListings = () => {
+const JobListings = ({isHome=false}) => {
+  const jobList = isHome
+
   return (
     <section className="bg-blue-50 px-4 py-10">
     <div className="container-xl lg:container m-auto">
