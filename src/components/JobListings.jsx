9f32e1d87017d2e155cd060484d4a73acebc0835
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 15:20:53 +0000

    Working on loading component

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,49 +1,49 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
-import JobListing from '../components/JobListing'
-import Loader from '../components/Loader';
+import { useEffect, useState } from "react";
+import JobListing from "../components/JobListing";
+import Loader from "../components/Loader";
 
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-const JobListings = ({isHome=false}) => {
-  const [jobs, setJobs] = useState([])
-  const [loading, setLoading] = useState(false)
-
-  useEffect(()=>{
-    const fetchJobs = async()=>{
+  useEffect(() => {
+    const fetchJobs = async () => {
       try {
-        const res = await fetch('http://localhost:5000/jobs')
-      const data = await res.json()
-      setJobs(data)
+        const res = await fetch("http://localhost:5000/jobs");
+        const data = await res.json();
+        setJobs(data);
       } catch (error) {
         console.log("Error fetching data!!", error);
-      }finally{
-        setLoading(true)
+      } finally {
+        setLoading(true);
       }
-      
-    }
-    fetchJobs()
-  },[])
+    };
+    fetchJobs();
+  }, []);
 
-  console.log(jobs)
+  console.log(jobs);
 
-  const jobList = isHome ? jobs.slice(0,3): jobs;
+  const jobList = isHome ? jobs.slice(0, 3) : jobs;
 
   return (
     <section className="bg-blue-50 px-4 py-10">
-    <div className="container-xl lg:container m-auto">
-      <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        {isHome ? 'Recent Jobs': 'Browse Jobs'}
-      </h2>
-        {loading ? (<Loader loading={loading}/>):(
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
+        </h2>
+        {loading ? (
+          <Loader loading={loading} />
+        ) : (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobList.map((job)=>(
-            <JobListing key={job.id} job={job}/>
+            {jobList.map((job) => (
+              <JobListing key={job.id} job={job} />
             ))}
           </div>
-        )} 
-    </div>
-  </section>
-  )
-}
+        )}
+      </div>
+    </section>
+  );
+};
 
-export default JobListings
+export default JobListings;
