df1796e08e060ce421e665164203633d1ce6dcb6
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 14:47:23 +0000

    Working on loading component

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -34,16 +34,13 @@ const JobListings = ({isHome=false}) => {
       <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
         {isHome ? 'Recent Jobs': 'Browse Jobs'}
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {loading ? (<Loader/>):(
-          <>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {jobList.map((job)=>(
             <JobListing key={job.id} job={job}/>
             ))}
-          </>
-        )}
-        
-      </div>
+          </div>
+        )} 
     </div>
   </section>
   )
