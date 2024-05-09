e0403a40e4cad48dac9e06919b9e59e7990fdf0a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 08:58:57 +0000

    Working on active links in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
               <div className="flex space-x-2">
                 <NavLink
                   to="/"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={({isActive})=> isActive ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2':'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'}
                 >
                   Home
                 </NavLink>
