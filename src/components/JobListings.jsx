c24a2b891b3ffe979e02d86b5651de458e7313d2
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-09 16:09:42 +0000

    Setting up proxy for api

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -10,7 +10,7 @@ const JobListings = ({ isHome = false }) => {
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const res = await fetch("/ai/jobs");
+        const res = await fetch("/api/jobs");
         const data = await res.json();
         setJobs(data);
       } catch (error) {
