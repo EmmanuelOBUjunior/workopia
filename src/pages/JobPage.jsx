441d39899eee1993447ec9daefedc57a3eeea551
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-10 10:27:11 +0000

    Working on individual job page

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react"
+import { useEffect } from "react"
+import { useSearchParams } from "react-router-dom"
 
 const JobPage = () => {
+    const id = useSearchParams('id')
+    const [jobData, setjobData] = useState([]);
+    useEffect(()=>{
+
+    }, [])
+
+
   return (
     <div>JobPage</div>
   )
