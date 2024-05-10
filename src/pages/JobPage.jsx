b1d44af50c8aaeae6a25714c52943743654c57f6
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-10 10:45:01 +0000

    Working on individual job page

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -4,22 +4,7 @@ import { useParams } from "react-router-dom";
 
 
 const JobPage = () => {
-    const params = useParams()
-    const id  = +params.id
-    console.log(id)
-    const [jobData, setjobData] = useState([]);
-    useEffect(()=>{
-        const fetchData = async() =>{
-            const res = await fetch(`/api/jobs/${id}`)
-            const data = await res.json()
-            setjobData(data)
-        }
-
-        fetchData()
-    }, [id])
-
-    console.log(jobData)
-
+    
   return (
     <div>Job Page for job {id}</div>
   )
