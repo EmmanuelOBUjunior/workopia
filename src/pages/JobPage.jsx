92ba624ce2b57acc36309f2dde1ace4e970db3fa
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-10 11:06:55 +0000

    Working on individual job page

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -11,7 +11,7 @@ const JobPage = () => {
       <section>
         <div className="container m-auto py-6 px-6">
           <Link
-            href="/jobs.html"
+            to="/jobs"
             className="text-indigo-500 hover:text-indigo-600 flex items-center"
           >
             <i className="fas fa-arrow-left mr-2"></i> Back to Job Listings
@@ -84,7 +84,7 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  href="/add-job.html"
+                  to="/add-job.html"
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
