1b3dd03abd6a771fbedefb5ebf39333b3ee34c52
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-15 07:06:37 +0000

    Working on edit job page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import AddJobs from "./pages/AddJobs";
 import JobPage,{jobLoader} from "./pages/JobPage";
+import Editjob from "./pages/EditJob";
 
 
 
@@ -44,7 +45,7 @@ const App = () => {
         <Route path="/jobs" element={<JobsPage/>}/>
         <Route path="/add-job" element={<AddJobs submitNewJob={submitNewJob}/>}/>
         <Route path="/job/:id" element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
-        <Route path="/job/:id" element={<JobPage loader={jobLoader}/>
+        <Route path="/job/:id" element={<EditJob loader={jobLoader}/>
         <Route path="*" element={<NotFoundPage/>}/>
       </Route>
     )
