721e15db22097634452b450cc24ef0b92a46522b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-15 07:15:09 +0000

    Working on edit job page

--- src/pages/Editjob.jsx
+++ src/pages/Editjob.jsx
@@ -3,7 +3,19 @@ import {useNavigate} from 'react-router-dom'
 import { toast } from "react-toastify";
 
 
+
 const EditJob = () => {
+
+  const [title, setTitle] = useState('');
+  const [type, setType] = useState('Full-Time');
+  const [location, setLocation] = useState('');
+  const [description, setDescription] = useState('');
+  const [salary, setSalary] = useState('Under $50K');
+  const [companyName, setCompanyName] = useState('');
+  const [companyDescription, setCompanyDescription] = useState('');
+  const [contactEmail, setContactEmail] = useState('');
+  const [contactPhone, setContactPhone] = useState('');
+
   return (
     <div>Editjob</div>
   )
