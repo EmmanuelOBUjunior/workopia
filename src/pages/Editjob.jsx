2a6888b4e1322a653e150db109c2c5354d5a62dc
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-15 07:26:17 +0000

    Working on edit job page

--- src/pages/Editjob.jsx
+++ src/pages/Editjob.jsx
@@ -33,7 +33,7 @@ const EditJob = ({submitJob}) => {
           <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
             <form onSubmit={handleSubmit}>
               <h2 className="text-3xl text-center font-semibold mb-6">
-                Add Job
+                Edit Job
               </h2>
 
               <div className="mb-4">
