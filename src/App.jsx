0cd15dccd43fecd97190cc9582a110b1209193e0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-15 07:07:38 +0000

    Working on edit job page

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ const App = () => {
         <Route path="/jobs" element={<JobsPage/>}/>
         <Route path="/add-job" element={<AddJobs submitNewJob={submitNewJob}/>}/>
         <Route path="/job/:id" element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
-        <Route path="/job/:id" element={<EditJob loader={jobLoader}/>
+        <Route path="/edit-job/:id" element={<EditJob loader={jobLoader}/>
         <Route path="*" element={<NotFoundPage/>}/>
       </Route>
     )
