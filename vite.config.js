0f75a3d72a48c7cd599ab80cdce50b3b2bb76def
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-28 07:38:30 +0000

    update:working on backend

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    // port: 3000,
-    proxy:{
-      '/api':{
-        target: 'https://job-api-ri32.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  // server:{
+  //   // port: 3000,
+  //   proxy:{
+  //     '/api':{
+  //       target: 'https://job-api-ri32.onrender.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
 })
